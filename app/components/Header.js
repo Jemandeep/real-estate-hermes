c50fa2a46f5d330fe713cf8088313f731336d4e8
Jemandeep <141459701+Jemandeep@users.noreply.github.com>
2024-09-11 21:31:19 -0600

    Fix

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,7 @@
 const Header = () => {
     return (
       <header className=" text-white py-20 text-center">
+<<<<<<< Updated upstream
         <h1 className="text-4xl font-bold mb-4">Discover Your Dream Home</h1>
         <p className="text-lg mb-6">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.
@@ -10,6 +11,11 @@ const Header = () => {
         <div className="space-x-4">
           <a href="/listings" className="bg-white text-stone-600 font-semibold px-6 py-3 rounded-md hover:bg-gray-100">Browse Listings</a>
         </div>
+=======
+        <h1 className="text-4xl font-bold mb-4">Find Your Dream Home</h1>
+        <p className="text-lg mb-6">Explore the best real estate opportunities with up-to-date market insights.</p>
+        <a href="/listings" className="bg-white text-blue-600 font-semibold px-6 py-3 rounded-md hover:bg-gray-100">Browse Listings</a>
+>>>>>>> Stashed changes
       </header>
     );
   }
