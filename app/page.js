eeaaf2995bb21f62eb87d1a176f908cdca34d515
Jemandeep <141459701+Jemandeep@users.noreply.github.com>
2024-10-16 14:15:43 -0600

    Removed mock api

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
-import ListingCard from "./listings/ListingCard";
+
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
@@ -67,13 +67,7 @@ const HomePage = () => {
       <div className="container mx-auto py-10 px-4">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-3xl font-bold text-gray-800">Featured Listings</h1>
-          {/* Filter Button */}
-          <button
-            onClick={() => setShowModal(true)}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          >
-            Filter
-          </button>
+          
         </div>
 
         {loading ? (
