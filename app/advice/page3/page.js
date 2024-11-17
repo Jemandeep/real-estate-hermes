d580886ef5a51fd47d66c0d35e08654f7b292003
Manav202 <141389168+Manav202@users.noreply.github.com>
2024-11-17 13:07:15 +0530

    Advice page3 fix

--- app/advice/page3/page.js
+++ app/advice/page3/page.js
@@ -1,4 +1,4 @@
-// pages/page.js
+// pages/page3.js
 "use client";
 import React from 'react';
 import NavBar from '@/app/components/NavBar';
@@ -20,29 +20,39 @@ const Page3 = () => {
       description: "It's easy to get carried away when house hunting. Stick to your budget to avoid financial strain.",
       imageSrc: "/images/p33.jpg", // Assuming another image for this tip
     },
+    {
+      title: "Hire a Home Inspector",
+      description: "A home inspection can uncover hidden issues with the property, saving you from unexpected costs in the future.",
+      imageSrc: "/images/p34.jpg", // New image for this tip
+    },
+    {
+      title: "Understand Closing Costs",
+      description: "Don't forget about closing costs. These can include appraisal fees, title insurance, and other expenses that aren't part of the home price.",
+      imageSrc: "/images/p35.jpg", // New image for this tip
+    },
   ];
 
   return (
-    <>
-      <NavBar />
-      <div className="container mx-auto p-10 pt-32">
-        <h1 className="text-3xl font-bold mb-4">First Time Buyers</h1>
-        <p className="text-lg mb-4">
-          Here are some key tips for first-time homebuyers to make the process smoother:
-        </p>
-        <div className="flex flex-col space-y-6">
-          {firstTimeBuyerTips.map((tip, index) => (
-            <div key={index} className="flex items-center bg-white rounded-lg shadow-lg p-4">
-              <img className="w-1/3 h-48 object-cover rounded-lg" src={tip.imageSrc} alt={tip.title} />
-              <div className="ml-4">
-                <h2 className="text-xl font-bold">{tip.title}</h2>
-                <p className="text-gray-700">{tip.description}</p>
-              </div>
-            </div>
-          ))}
+      <>
+        <NavBar />
+        <div className="container mx-auto p-10 pt-32">
+          <h1 className="text-3xl font-bold mb-4">First Time Buyers</h1>
+          <p className="text-lg mb-4">
+            Here are some key tips for first-time homebuyers to make the process smoother:
+          </p>
+          <div className="flex flex-col space-y-6">
+            {firstTimeBuyerTips.map((tip, index) => (
+                <div key={index} className="flex items-center bg-white rounded-lg shadow-lg p-4">
+                  <img className="w-1/3 h-48 object-cover rounded-lg" src={tip.imageSrc} alt={tip.title} />
+                  <div className="ml-4">
+                    <h2 className="text-xl font-bold">{tip.title}</h2>
+                    <p className="text-gray-700">{tip.description}</p>
+                  </div>
+                </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </>
+      </>
   );
 };
 
